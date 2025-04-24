84a2e00bb924095e9ff023a3f7aa25fada6e023a
Jade Chen <jadeeechen@gmail.com>
2025-04-23 18:26:39 -0700

    practice post-interrogation

--- data-validation-practice.R
+++ data-validation-practice.R
@@ -40,3 +40,28 @@ agent <- small_table |>
   col_vals_between(d, left=0, right = 10000, inclusive=c(TRUE,TRUE)) |>
   interrogate()
 
+##### POST-INTERROGATION #####
+
+# create agent that fails
+agent_fail <- small_table |>
+  create_agent() |>
+  col_vals_lt(a, value = 10) |>
+  col_vals_between(d, left = 0, right = 5000) |>
+  col_vals_in_set(f, set = c("low", "mid", "high")) |>
+  col_vals_regex(b, regex = "^[0-9]-[a-z]{3}-[0-9]{3}$") |>
+  interrogate()
+
+# see agent_fail in viewer
+agent_fail
+
+# see passes from agent_fail
+get_sundered_data(agent_fail, type = "pass")
+
+# see fails from agent_fail
+get_sundered_data(agent_fail, type = "fail")
+
+# see both passess/fails from agent_fail
+get_sundered_data(agent_fail, type = "combined")
+
+# see step-by-step breakdown of failed rows (per validation rule)
+get_data_extracts(agent_fail)
