31a97aface75a1fc986a2fca7786a7f17b66302c
Jade Chen <jadeeechen@gmail.com>
2025-04-23 17:58:51 -0700

    practice validation rules

---
+++ data-validation-practice.R
@@ -0,0 +1,23 @@
+library(pointblank)
+data(small_table)
+small_table
+
+
+##### VALIDATION RULES #####
+
+# single validation: checking if all a's are less than 4 -> NO
+small_table |>
+  col_vals_lt(a, value = 4)
+
+# single validation: checking if all a's are more than 10 -> NO
+small_table |>
+  col_vals_gt(a, value = 10)
+
+# single validation: checking if all a's are less than 10 -> YES
+small_table |>
+  col_vals_lt(a, value = 10)
+
+# chaining validations:
+small_table |>
+  col_vals_lt(a, value = 10) |>
+  col_vals_between(d, left=0, right = 10000, inclusive=c(TRUE,TRUE))
