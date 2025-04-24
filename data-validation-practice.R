31bae3a192d26221f9eb1c4c310ba03c92b45e64
Jade Chen <jadeeechen@gmail.com>
2025-04-23 18:27:19 -0700

    practice non-interactive validation checks

--- data-validation-practice.R
+++ data-validation-practice.R
@@ -65,3 +65,14 @@ get_sundered_data(agent_fail, type = "combined")
 
 # see step-by-step breakdown of failed rows (per validation rule)
 get_data_extracts(agent_fail)
+
+##### PIPELINE DATA VALIDATION #####
+
+# to run validation checks non-interactively
+small_table %>%
+  col_is_posix(date_time) %>%
+  col_vals_in_set(f, set = c("low", "mid", "high")) %>%
+  col_vals_lt(a, value = 10) %>%
+  col_vals_regex(b, regex = "^[0-9]-[a-z]{3}-[0-9]{3}$") %>%
+  col_vals_between(d, left = 0, right = 5000)
+
